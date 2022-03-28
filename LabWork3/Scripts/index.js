a8f501ddc590028fab81ae775669e4b5d82f0ec2
Kerzolys <kerzolys@gmail.com>
2022-03-28 14:22:03 +0300

    forget to write code for changing info of images. done it"

--- LabWork3/Scripts/index.js
+++ LabWork3/Scripts/index.js
@@ -1,14 +1,27 @@
 let images = [{
         src: './asset/image1.png',
-        title: 'Rostov-on-Don. Admiral'
+        title: 'Rostov-on-Don. Admiral',
+        infoCity: 'Rostov-on-Don LCD admiral',
+        infoArea: '81 m2',
+        infoTime: '3.5 months',
+        infoCost: 'Upon request'
+
     },
     {
         src: './asset/image2.png',
-        title: 'Sochi. Thieves'
+        title: 'Sochi. Thieves',
+        infoCity: 'Sochi Thieves',
+        infoArea: '105 m2',
+        infoTime: '4 months',
+        infoCost: 'Upon request'
     },
     {
         src: './asset/image3.png',
-        title: 'Rostov-on-Don. Patriotic'
+        title: 'Rostov-on-Don. Patriotic',
+        infoCity: 'Rostov-on-Don Patriotic',
+        infoArea: '93 m2',
+        infoTime: '3 months',
+        infoCost: 'Upon request'
     }
 ]
 
@@ -19,8 +32,11 @@ function initSlider() {
     let sliderArrows = document.querySelector('.js-arrows');
     let sliderDots = document.querySelector('.js-dots');
     let sliderTitles = document.querySelector('.js-titles')
+    let sliderInfoCity = document.querySelector('.js-info-city')
+    let sliderInfoArea = document.querySelector('.js-info-area')
+    let sliderInfoTime = document.querySelector('.js-info-time')
+    let sliderInfoCost = document.querySelector('.js-info-cost')
 
-    console.log(sliderTitles);
 
     //разберем массив с изображениями, создадим элементы
     //и запишем их в <div js-slider_images>
@@ -32,7 +48,15 @@ function initSlider() {
 
     changeTitles();
 
-    initAutoplay();
+    initInfoCity();
+    initInfoArea();
+    initInfoTime();
+    initInfoCost();
+
+
+    // changeInfo();
+
+    // initAutoplay();
 
     function initImages() {
         images.forEach((image, index) => {
@@ -69,7 +93,10 @@ function initSlider() {
         sliderDots.querySelector('.n' + num).classList.add('active');
         sliderTitles.querySelector('.active').classList.remove('active')
         sliderTitles.querySelector('.n' + num).classList.add('active');
-
+        changeInfoCity(num);
+        changeInfoArea(num);
+        changeInfoTime(num);
+        changeInfoCost(num);
 
     }
 
@@ -90,23 +117,75 @@ function initSlider() {
     }
 
     function changeTitles() {
-        sliderTitles.querySelectorAll('.title').forEach((title, index) => {
-            console.log(title);
-            title.classList.add(`n${index}`)
-            title.dataset.index = `${index}`
+        sliderTitles.querySelectorAll('.js-title').forEach((title, index) => {
+            // console.log(title);
+            title.classList.add(`n${index}`);
+            title.dataset.index = `${index}`;
             title.addEventListener('click', function() {
                 moveSlider(this.dataset.index)
             })
         })
     }
 
-    function initAutoplay() {
-        setInterval(() => {
-            let curNumber = +sliderImages.querySelector('.active').dataset.index;
-            let nextNumber = curNumber === images.length - 1 ? 0 : curNumber + 1;
-            moveSlider(nextNumber)
-        }, 4000)
+    function initInfoCity() {
+        let infoSpan = `
+        <span>${images[0].infoCity}
+        </span>`
+
+        sliderInfoCity.innerHTML = infoSpan;
     }
+
+    function changeInfoCity(num) {
+        if (!images[num].infoCity) return;
+        sliderInfoCity.innerHTML = images[num].infoCity;
+    }
+
+    function initInfoArea() {
+        let infoSpan = `
+        <span>${images[0].infoArea}
+        </span>`
+
+        sliderInfoArea.innerHTML = infoSpan;
+    }
+
+    function changeInfoArea(num) {
+        if (!images[num].infoCity) return;
+        sliderInfoArea.innerHTML = images[num].infoArea;
+    }
+
+    function initInfoTime() {
+        let infoSpan = `
+        <span>${images[0].infoTime}
+        </span>`
+
+        sliderInfoTime.innerHTML = infoSpan;
+    }
+
+    function changeInfoTime(num) {
+        if (!images[num].infoTime) return;
+        sliderInfoTime.innerHTML = images[num].infoTime;
+    }
+
+    function initInfoCost() {
+        let infoSpan = `
+        <span>${images[0].infoCost}
+        </span>`
+
+        sliderInfoCost.innerHTML = infoSpan;
+    }
+
+    function changeInfoCost(num) {
+        if (!images[num].infoCost) return;
+        sliderInfoCost.innerHTML = images[num].infoCost;
+    }
+
+    // function initAutoplay() {
+    //     setInterval(() => {
+    //         let curNumber = +sliderImages.querySelector('.active').dataset.index;
+    //         let nextNumber = curNumber === images.length - 1 ? 0 : curNumber + 1;
+    //         moveSlider(nextNumber)
+    //     }, 4000)
+    // }
 }
 
 
